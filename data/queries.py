ba326f32b13776bc62e7a4db977fe699bd154555
Bambordzini <122827508+Bambordzini@users.noreply.github.com>
2023-06-17 20:15:31 +0200

    base
    base

--- data/queries.py
+++ data/queries.py
@@ -3,3 +3,37 @@ from data import data_manager
 
 def get_shows():
     return data_manager.execute_select('SELECT id, title FROM shows;')
+
+def get_most_rated_shows(number_of_elements_to_display, page_number):
+    print('numer: ', page_number)
+    offset_flag = number_of_elements_to_display * (int(page_number) - 1)
+    return data_manager.execute_select('''
+    SELECT shows.*, string_agg(genres.name, ', ') 
+    AS genre_names 
+    FROM shows 
+    LEFT JOIN show_genres ON shows.id = show_genres.show_id
+    JOIN genres ON show_genres.genre_id = genres.id
+    GROUP BY shows.id ORDER by rating DESC LIMIT %(limit)s OFFSET %(offset)s''', variables={'limit': number_of_elements_to_display, 'offset': offset_flag})
+
+
+def get_total_number_of_shows():
+     return data_manager.execute_select('SELECT COUNT(*) AS total_rows FROM shows')
+ 
+def get_show(id):
+    return data_manager.execute_select("""SELECT title, 
+     year, 
+     overview, 
+     runtime, 
+     trailer, 
+     homepage, 
+     string_agg(genres.name, ', ') AS genres_names, 
+     string_agg(actors.name, ', ') AS actors_names, 
+     rating FROM shows left join show_genres ON shows.id = show_genres.show_id
+     left join show_characters ON shows.id = show_characters.show_id
+     left JOIN genres ON show_genres.genre_id = genres.id
+     left JOIN actors ON show_characters.actor_id = actors.id WHERE shows.id = %(show_id)s GROUP BY shows.id""", variables={'show_id': id})
+
+    
+    
+
+     
--- main.py
+++ main.py
@@ -4,22 +4,52 @@ import math
 from dotenv import load_dotenv
 
 load_dotenv()
-app = Flask('codecool_series')
+app = Flask("codecool_series")
 
-@app.route('/')
+
+@app.route("/")
 def index():
     shows = queries.get_shows()
-    return render_template('index.html', shows=shows)
+    return render_template("index.html", shows=shows)
 
 
-@app.route('/design')
+
+
+@app.route("/design")
 def design():
-    return render_template('design.html')
+    return render_template("design.html")
+
+
+@app.route("/shows/most-rated")
+@app.route("/shows/most-rated/<int:page_number>")
+def shows_most_rated(page_number=1):
+    print(page_number)
+    number_of_elements_to_display = 15
+    total_number_of_shows = queries.get_total_number_of_shows()[0]["total_rows"]
+    most_rated_shows_list = queries.get_most_rated_shows(
+        number_of_elements_to_display, page_number
+    )
+    total_number_of_pages = math.ceil(
+        total_number_of_shows / number_of_elements_to_display
+    )
+    return render_template(
+        "most-rated-shows.html",
+        most_rated_shows_list=most_rated_shows_list,
+        total_number_of_pages=total_number_of_pages,
+        current_page=page_number,
+    )
+
+
+@app.route("/show/<int:id>")
+def show(id):
+    show_detail = queries.get_show(id)
+    print(show_detail)    
+    return render_template ("show_detail.html", show=show_detail)
 
 
 def main():
     app.run(debug=False)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
